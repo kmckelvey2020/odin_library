a68b0b10d62cdaa99af7651e1f0b6b3cd13cdd5b
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-07-05 15:36:00 -0400

    Added empty functions for the Book constructor, addBookToLibrary, removeBookFromLibrary, toggleReadStatus, and displayBooks according to project specifications. Added boilerplate html for book_form.html where the book entry form will be located.

---
+++ script.js
@@ -0,0 +1,28 @@
+let myLibrary = [];
+
+function Book() {
+    // the constructor...
+
+}
+
+// take user input and store the new book objects into an array
+function addBookToLibrary() {
+    //do stuff here
+
+}
+
+// remove book from array
+function removeBoomFromLibrary() {
+
+}
+
+// toggle read status from unread to read or vice versa
+function toggleReadStatus() {
+    
+}
+
+// loop through the array and display each book on the page
+function displayBooks() {
+
+}
+
