9823607eb3a9bf501ceccf70803cc0f25387de86
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-07-08 13:31:25 -0400

    Added event listeners to form and grabbing form submissions in a FormData object.

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function Book() {
 }
 
 // take user input and store the new book objects into an array
-function addBookToLibrary() {
+function addBookToLibrary(event) {
     //do stuff here
+    event.preventDefault();
 
+    const formData = new FormData(this);
+    formData.forEach((element) => {
+        console.log(element);
+    });
+    console.log(formData);
 }
 
 // remove book from array
@@ -36,3 +42,26 @@ function exportBooks() {
 
 }
 
+function searchForBook(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    formData.forEach((element) => {
+        console.log(element);
+    });
+    for(const [key, value] of formData) {
+        console.log(`${key}: ${value}`);
+    };
+    console.log(this);
+    console.log(formData.values);
+}
+
+function addListeners() {
+    const searchForm = document.getElementById("search_form");
+    const bookForm = document.getElementById("book_form");
+
+    searchForm.addEventListener("submit", searchForBook);
+    bookForm.addEventListener("submit", addBookToLibrary);
+}
+
+addListeners();
